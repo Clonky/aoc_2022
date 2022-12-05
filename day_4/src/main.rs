9d60dc1692849098115ef9d3ebcdace700f8ebca
Philip Schmid <philip.schmid@tum.de>
2022-12-05 14:55:47 +0100

    Update

---
+++ day_4/src/main.rs
@@ -0,0 +1,10 @@
+use std::collections::HashSet;
+use std::fs;
+
+fn read_
+
+fn main() {
+    const SRC: &str = "test.txt";
+    let contents = fs::read_to_string(SRC).expect("Couldn't read file");
+    let lines = contents.lines();
+}
